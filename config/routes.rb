a18da46692b78f2df4f903999dbd00e98837376a
Hayato Saito <omimisama@gmail.com>
2020-03-06 12:59:42 +0900

    #0_共通項目

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'facilities/new'
-  get 'static_pages/top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+
+  get '/signup', to: 'facilities#new'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :facilities
 end
