6ee4ace43eb69f4faa4299c5254e55f660e39b45
Hayato Saito <omimisama@gmail.com>
2020-03-07 20:01:40 +0900

    add_seed

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,7 +1,14 @@
 class FacilitiesController < ApplicationController
+  before_action :set_facility, only: [:show, :edit, :update]
+  before_action :logged_in_facility, only: [:index, :show, :edit, :update]
+  before_action :correct_facility, only: [:edit, :update]
+
+  def index
+    @facilities = Facility.all
+  end
+
   def show
     @facilities = Facility.all
-    @facility = Facility.find(params[:id])
   end
 
   def new
@@ -20,13 +27,42 @@ class FacilitiesController < ApplicationController
   end
 
   def edit
-    @facility = Facility.find(params[:id])
+  end
+
+  def update
+    if @facility.update_attributes(facility_params)
+      flash[:success] = "施設情報を更新しました。"
+      redirect_to @facility
+    else
+      render :edit
+    end
   end
 
   private
 
     def facility_params
       params.require(:facility).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeフィルター
+
+    # paramsハッシュから施設ユーザーを取得します。
+    def set_facility
+      @facility = Facility.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_facility
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインして下さい。"
+        redirect_to login_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_facility
+      redirect_to(root_url) unless current_facility?(@facility)
     end
 
 end
