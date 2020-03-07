701c5c0110416b8025dd5d93cc5fe1df9d5d0c77
Hayato Saito <omimisama@gmail.com>
2020-03-07 22:54:16 +0900

    施設ユーザーのリソース周り

--- db/migrate/20200307110108_add_admin_to_facilities.rb
+++ db/migrate/20200307110108_add_admin_to_facilities.rb
@@ -1,5 +1,5 @@
 class AddAdminToFacilities < ActiveRecord::Migration[5.2]
   def change
-    add_column :facilities, :admin, :boolean
+    add_column :facilities, :admin, :boolean, default: false
   end
 end
