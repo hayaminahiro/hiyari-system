b77f5532474cbd41f1c8805358653e45623da3aa
Hayato Saito <omimisama@gmail.com>
2020-03-08 23:19:25 +0900

    #15_施設利用者一覧ページの基本レイアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,12 @@ Facility.create!(name:  "特別養護老人ホーム ねこの街",
 
 #施設利用者作成
 a = 0
+b = 0
 60.times do
   Senior.create!(facility_id: 1,
                  senior_name: "#{a += 1}鱗滝 左近時",
                  floor: "2階",
-                 charge_worker: "#{a += 1}齋藤 隼人",
+                 charge_worker: "#{b += 1}齋藤 隼人",
                  using_flg: true
                  )
 end
