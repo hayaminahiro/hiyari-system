f5165bf52dd5b4c1f6f6138703b5eb5c80c19051
Hayato Saito <omimisama@gmail.com>
2020-03-18 16:59:29 +0900

    #29_職員一覧ページのレイアウト

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,7 +1,12 @@
 class WorkersController < ApplicationController
 
+  #職員一覧ページ
   def index
-
+    @facility = Facility.find(params[:facility_id])
+    @workers2f = Worker.where(working_floor: 2).where(working_flg: true).order(:worker_name_call)
+    @workers3f = Worker.where(working_floor: 3).where(working_flg: true).order(:worker_name_call)
+    @workers4f = Worker.where(working_floor: 4).where(working_flg: true).order(:worker_name_call)
+    @workers_off = Worker.where(working_flg: false).order(:worker_name_call)
   end
 
 end
