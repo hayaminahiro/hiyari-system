0e9b8bd3b11c7622cc4d4ecb41389f0879ebcc6d
Hayato Saito <omimisama@gmail.com>
2020-03-20 22:43:26 +0900

    #35_中間モデル作成

--- app/controllers/seniors_controller.rb
+++ app/controllers/seniors_controller.rb
@@ -36,7 +36,7 @@ class SeniorsController < ApplicationController
     @facility = Facility.find(params[:facility_id])
     @senior = @facility.seniors.find(params[:id])
     #https://qiita.com/Kohei_Kishimoto0214/items/cb9a3d3da57708fb52c9 多対多呼び出し参照
-    @workers = current_facility.workers.where(working_flg: true).includes(:facility)
+    @workers = @facility.workers.where(working_flg: true).includes(:facility)
   end
 
   #施設利用者情報編集
