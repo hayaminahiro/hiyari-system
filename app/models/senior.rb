613beaa47e2943fb5e039c21794a573d018b467a
Hayato Saito <omimisama@gmail.com>
2020-03-22 07:39:18 +0900

    #37_利用者テーブルの中間テーブルからデータ取り出し

--- app/models/senior.rb
+++ app/models/senior.rb
@@ -1,7 +1,7 @@
 class Senior < ApplicationRecord
   belongs_to :facility
 
-  has_many :senior_workers
+  has_many :senior_workers, dependent: :destroy
   has_many :workers, through: :senior_workers
 
   accepts_nested_attributes_for :senior_workers
