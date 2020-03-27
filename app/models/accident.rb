05896ad98779ac998c7f50f3d5e981093af71204
Hayato Saito <omimisama@gmail.com>
2020-03-27 11:20:58 +0900

    #41_各階のヒヤリ・事故seed　追加

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -2,8 +2,8 @@ class Accident < ApplicationRecord
   belongs_to :senior
 
   validates :which_accident, presence: true
-  validates :reporter, presence: true
+  #validates :reporter, presence: true
   validates :accident_datetime, presence: true
-  validates :accident_senior, presence: true
+  #validates :accident_senior, presence: true
   validates :accident_scene, presence: true
 end
