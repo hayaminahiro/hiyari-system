a3397764e85966266af958ee8b98e07c3b355b94
Hayato Saito <omimisama@gmail.com>
2020-04-08 16:29:14 +0900

    #49_ヒヤリ新規作成基本項目

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_063003) do
+ActiveRecord::Schema.define(version: 2020_04_08_061310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,13 +18,14 @@ ActiveRecord::Schema.define(version: 2020_03_27_063003) do
   create_table "accidents", force: :cascade do |t|
     t.string "which_accident"
     t.string "reporter"
-    t.datetime "accident_datetime", default: "2020-03-31 22:30:00"
+    t.datetime "accident_datetime"
     t.string "accident_senior"
     t.string "accident_scene"
     t.bigint "senior_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "accident_floor"
+    t.datetime "accident_time"
     t.index ["senior_id"], name: "index_accidents_on_senior_id"
   end
 
