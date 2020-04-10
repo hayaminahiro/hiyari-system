a7df6a83204e958ff2545d3bda84fa5ad65b7b5c
Hayato Saito <omimisama@gmail.com>
2020-04-10 17:30:03 +0900

    #49_time_selectに切り替え

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -1,9 +1,9 @@
 class Accident < ApplicationRecord
   belongs_to :senior
 
-  validates :which_accident, presence: true
+  #validates :which_accident, presence: true
   #validates :reporter, presence: true
-  #validates :accident_datetime, presence: true
+  validates :accident_time, presence: true
   #validates :accident_senior, presence: true
   validates :accident_scene, presence: true
 
