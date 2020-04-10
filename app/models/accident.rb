9cb2d51975754a4e4861437496e0e5d196400f4b
Hayato Saito <omimisama@gmail.com>
2020-04-10 16:34:06 +0900

    #49_バリデーションエラー後の初期値設定

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -1,17 +1,12 @@
 class Accident < ApplicationRecord
   belongs_to :senior
-  #belongs_to :senior, optional: true
 
   validates :which_accident, presence: true
   #validates :reporter, presence: true
   #validates :accident_datetime, presence: true
   #validates :accident_senior, presence: true
-  #validates :accident_scene, presence: true
+  validates :accident_scene, presence: true
 
   enum accident_result: {ヒヤリのみ:1, 軽事故等:2}
 
 end
-
-
-#validates :account_id, presence: true
-#validates :account, presence: true, if: -> { account_id.present? }
