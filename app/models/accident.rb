f180ab18466bfb37bbd4591358042dedfe9a35e9
Hayato Saito <omimisama@gmail.com>
2020-04-11 07:28:18 +0900

    #49_ヒヤリ新規作成、text_areaへ変更

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -1,10 +1,8 @@
 class Accident < ApplicationRecord
   belongs_to :senior
 
-  #validates :which_accident, presence: true
-  #validates :reporter, presence: true
+  validates :which_accident, presence: true
   validates :accident_time, presence: true
-  #validates :accident_senior, presence: true
   validates :accident_scene, presence: true
 
   enum accident_result: {ヒヤリのみ:1, 軽事故等:2}
