c9a29c93658573500b1ca8a3670c551dd054ca5e
Hayato Saito <omimisama@gmail.com>
2020-04-12 09:44:14 +0900

    #49_ヒヤリ新規作成の場面カラム追加

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -7,4 +7,8 @@ class Accident < ApplicationRecord
 
   enum accident_result: {ヒヤリのみ:1, 軽事故等:2}
 
+  enum activity_scene:{
+      通所・送迎: 0, 活動・支援（施設内・外出）: 1, 給食・配膳: 2, トイレ・排泄: 3, 入浴: 4, その他: 5
+  }
+
 end
--- db/migrate/20200409022652_add_activity_scene_to_accidents.rb
+++ db/migrate/20200409022652_add_activity_scene_to_accidents.rb
@@ -1,5 +1,5 @@
 class AddActivitySceneToAccidents < ActiveRecord::Migration[5.2]
   def change
-    add_column :accidents, :activity_scene, :string
+    add_column :accidents, :activity_scene, :integer
   end
 end
