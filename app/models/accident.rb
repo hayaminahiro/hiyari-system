bd697c42fd35ac2eb035e444a15c45a35fa0de1b
Hayato Saito <omimisama@gmail.com>
2020-04-16 10:17:27 +0900

    #58_ヒヤリ新規作成の値保時の修正

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -2,7 +2,8 @@ class Accident < ApplicationRecord
   belongs_to :senior
 
   validates :which_accident, presence: true
-  validates :accident_time, presence: true
+  validates :department, presence: true
+  validates :reporter, presence: true
   validates :accident_scene, presence: true
 
   enum accident_result: {ヒヤリのみ:1, 軽事故等:2}
