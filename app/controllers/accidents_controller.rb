160fe54b3d76041e0450614df898a1892a59d0ad
Hayato Saito <omimisama@gmail.com>
2020-04-29 15:23:31 +0900

    #66_ヒヤリ集計ページの他害・自傷行為：モデル移行

--- app/controllers/accidents_controller.rb
+++ app/controllers/accidents_controller.rb
@@ -124,7 +124,7 @@ class AccidentsController < ApplicationController
     @meal_hat2f_19_22, @meal_hat3f_19_22, @meal_hat4f_19_22,
     @meal_hat2f_22_3,  @meal_hat3f_22_3, @meal_hat4f_22_3,
     @meal_hat2f_3_7, @meal_hat3f_3_7, @meal_hat4f_3_7,
-    # 薬関連
+    #薬関係
     @medicine_hat2f_7_9, @medicine_hat3f_7_9, @medicine_hat4f_7_9,
     @medicine_hat2f_9_12, @medicine_hat3f_9_12, @medicine_hat4f_9_12,
     @medicine_hat2f_12_14, @medicine_hat3f_12_14, @medicine_hat4f_12_14,
@@ -132,7 +132,16 @@ class AccidentsController < ApplicationController
     @medicine_hat2f_17_19, @medicine_hat3f_17_19, @medicine_hat4f_17_19,
     @medicine_hat2f_19_22, @medicine_hat3f_19_22, @medicine_hat4f_19_22,
     @medicine_hat2f_22_3,  @medicine_hat3f_22_3, @medicine_hat4f_22_3,
-    @medicine_hat2f_3_7, @medicine_hat3f_3_7, @medicine_hat4f_3_7 = Accident.time_division(accidents)
+    @medicine_hat2f_3_7, @medicine_hat3f_3_7, @medicine_hat4f_3_7,
+    #他害・自傷行為
+    @scratch_hat2f_7_9, @scratch_hat3f_7_9, @scratch_hat4f_7_9,
+    @scratch_hat2f_9_12, @scratch_hat3f_9_12, @scratch_hat4f_9_12,
+    @scratch_hat2f_12_14, @scratch_hat3f_12_14, @scratch_hat4f_12_14,
+    @scratch_hat2f_14_17, @scratch_hat3f_14_17, @scratch_hat4f_14_17,
+    @scratch_hat2f_17_19, @scratch_hat3f_17_19, @scratch_hat4f_17_19,
+    @scratch_hat2f_19_22, @scratch_hat3f_19_22, @scratch_hat4f_19_22,
+    @scratch_hat2f_22_3,  @scratch_hat3f_22_3, @scratch_hat4f_22_3,
+    @scratch_hat2f_3_7, @scratch_hat3f_3_7, @scratch_hat4f_3_7 = Accident.time_division(accidents)
 
 
     #転倒・転落のヒヤリハット
