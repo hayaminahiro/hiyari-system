9a2bb79b0fe749d0220814aef427aa06cc60880b
Hayato Saito <omimisama@gmail.com>
2020-05-02 05:54:55 +0900

    #66_ヒヤリ集計ページの場面総合計：モデル移行

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -934,6 +934,39 @@ class Accident < ApplicationRecord
     other_scene_hat3f_3_7 = []
     other_scene_hat4f_3_7 = []
 
+    #7~9時の場面総合計ヒヤリ
+    total_scene_hat2f_7_9 = []
+    total_scene_hat3f_7_9 = []
+    total_scene_hat4f_7_9 = []
+    #9~12時の場面総合計ヒヤリ
+    total_scene_hat2f_9_12 = []
+    total_scene_hat3f_9_12 = []
+    total_scene_hat4f_9_12 = []
+    #12~14時の場面総合計ヒヤリ
+    total_scene_hat2f_12_14 = []
+    total_scene_hat3f_12_14 = []
+    total_scene_hat4f_12_14 = []
+    #14~17時の場面総合計ヒヤリ
+    total_scene_hat2f_14_17 = []
+    total_scene_hat3f_14_17 = []
+    total_scene_hat4f_14_17 = []
+    #17~19時の場面総合計ヒヤリ
+    total_scene_hat2f_17_19 = []
+    total_scene_hat3f_17_19 = []
+    total_scene_hat4f_17_19 = []
+    #19~22時の場面総合計ヒヤリ
+    total_scene_hat2f_19_22 = []
+    total_scene_hat3f_19_22 = []
+    total_scene_hat4f_19_22 = []
+    #22~3時の場面総合計ヒヤリ
+    total_scene_hat2f_22_3 = []
+    total_scene_hat3f_22_3 = []
+    total_scene_hat4f_22_3 = []
+    #3~7時の場面総合計ヒヤリ
+    total_scene_hat2f_3_7 = []
+    total_scene_hat3f_3_7 = []
+    total_scene_hat4f_3_7 = []
+
     accidents.each do |hat|
       #7~9時の転倒・転落ヒヤリ
       if time_range_7_9(hat) && hat.floor2 && hat.hat && hat.fall
@@ -2210,6 +2243,64 @@ class Accident < ApplicationRecord
       elsif time_range_3_7(hat) && hat.floor4 && hat.hat && hat.other_scene
         other_scene_hat4f_3_7 << hat
       end
+
+      #7~9時の場面総合計ヒヤリ
+      if time_range_7_9(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_7_9 << hat
+      elsif time_range_7_9(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_7_9 << hat
+      elsif time_range_7_9(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_7_9 << hat
+        #9~12時の場面総合計ヒヤリ
+      elsif time_range_9_12(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_9_12 << hat
+      elsif time_range_9_12(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_9_12 << hat
+      elsif time_range_9_12(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_9_12 << hat
+        #12~14時の場面総合計ヒヤリ
+      elsif time_range_12_14(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_12_14 << hat
+      elsif time_range_12_14(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_12_14 << hat
+      elsif time_range_12_14(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_12_14 << hat
+        #14~17時の場面総合計ヒヤリ
+      elsif time_range_14_17(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_14_17 << hat
+      elsif time_range_14_17(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_14_17 << hat
+      elsif time_range_14_17(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_14_17 << hat
+        #17~19時の場面総合計ヒヤリ
+      elsif time_range_17_19(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_17_19 << hat
+      elsif time_range_17_19(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_17_19 << hat
+      elsif time_range_17_19(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_17_19 << hat
+        #19~22時の場面総合計ヒヤリ
+      elsif time_range_19_22(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_19_22 << hat
+      elsif time_range_19_22(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_19_22 << hat
+      elsif time_range_19_22(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_19_22 << hat
+        #22~3時の場面総合計ヒヤリ
+      elsif time_range_22_3(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_22_3 << hat
+      elsif time_range_22_3(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_22_3 << hat
+      elsif time_range_22_3(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_22_3 << hat
+        #3~7時の場面総合計ヒヤリ
+      elsif time_range_3_7(hat) && hat.floor2 && hat.hat
+        total_scene_hat2f_3_7 << hat
+      elsif time_range_3_7(hat) && hat.floor3 && hat.hat
+        total_scene_hat3f_3_7 << hat
+      elsif time_range_3_7(hat) && hat.floor4 && hat.hat
+        total_scene_hat4f_3_7 << hat
+      end
     end
 
     [
@@ -2586,6 +2677,23 @@ class Accident < ApplicationRecord
     other_scene_hat2f_22_3, other_scene_hat3f_22_3, other_scene_hat4f_22_3,
     #返り値：3~7時のその他ヒヤリ
     other_scene_hat2f_3_7, other_scene_hat3f_3_7, other_scene_hat4f_3_7,
+
+    #返り値：7~9時の場面総合計ヒヤリ
+    total_scene_hat2f_7_9, total_scene_hat3f_7_9, total_scene_hat4f_7_9,
+    #返り値：9~12時の場面総合計ヒヤリ
+    total_scene_hat2f_9_12, total_scene_hat3f_9_12, total_scene_hat4f_9_12,
+    #返り値：12~14時の場面総合計ヒヤリ
+    total_scene_hat2f_12_14, total_scene_hat3f_12_14, total_scene_hat4f_12_14,
+    #返り値：14~17時の場面総合計ヒヤリ
+    total_scene_hat2f_14_17, total_scene_hat3f_14_17, total_scene_hat4f_14_17,
+    #返り値：17~19時の場面総合計ヒヤリ
+    total_scene_hat2f_17_19, total_scene_hat3f_17_19, total_scene_hat4f_17_19,
+    #返り値：19~22時の場面総合計ヒヤリ
+    total_scene_hat2f_19_22, total_scene_hat3f_19_22, total_scene_hat4f_19_22,
+    #返り値：22~3時の場面総合計ヒヤリ
+    total_scene_hat2f_22_3, total_scene_hat3f_22_3, total_scene_hat4f_22_3,
+    #返り値：3~7時の場面総合計ヒヤリ
+    total_scene_hat2f_3_7, total_scene_hat3f_3_7, total_scene_hat4f_3_7
     ]
   end
 
