2310b8417afc190ba51f11b31a8c7c6d9413e542
Hayato Saito <omimisama@gmail.com>
2020-05-04 14:11:52 +0900

    #70_職員サイン欄のカラム、真偽値へ変更

--- db/migrate/20200409023155_add_charge_sign_to_accidents.rb
+++ db/migrate/20200409023155_add_charge_sign_to_accidents.rb
@@ -1,5 +1,5 @@
 class AddChargeSignToAccidents < ActiveRecord::Migration[5.2]
   def change
-    add_column :accidents, :charge_sign, :string
+    add_column :accidents, :charge_sign, :boolean
   end
 end
