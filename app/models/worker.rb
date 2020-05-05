cc8db0174450426ded8ab1fe053b559fa3cbd936
Hayato Saito <omimisama@gmail.com>
2020-05-06 00:18:49 +0900

    #70_enum-Workerモデル役職追加

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -12,5 +12,10 @@ class Worker < ApplicationRecord
   validates :worker_name_call, length: { in: 1..25 }, format: { with: VALID_WORKER_NAME_CALL_REGEX }, allow_blank: true
   validates :sign_name, presence: true, length: { in: 1..6 }
 
+  #役職選択
+  enum position: {
+      なし: 0, ２F主任: 1, ３F主任: 2, ４F主任: 3, ２F係長: 4, ３F係長: 5, ４F係長: 6, リスマネ: 7, 次長: 8, 施設長: 9
+  }
+
 end
 
