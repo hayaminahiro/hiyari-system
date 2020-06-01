7703a7c521689fa6c86e52b999376b010c6c933a
Hayato Saito <omimisama@gmail.com>
2020-06-01 13:13:18 +0900

    #88_ユーザー別Top画面修正

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -15,7 +15,7 @@ class WorkersController < ApplicationController
   #職員新規作成モーダル
   def new_worker
     @worker = Worker.new
-    @seniors = Senior.using.including_facility
+    @seniors = Senior.using.including_facility.current_facility(current_facility)
   end
 
   #職員新規作成
@@ -23,7 +23,11 @@ class WorkersController < ApplicationController
     @worker = @facility.workers.new(worker_params)
     if worker_valid?((worker_params)[:worker_name], (worker_params)[:senior_ids])
       @worker.save
-      flash[:success] = "職員「#{@worker.worker_name}」さんを新規登録しました。ふりがなに全角空白と半角英数字は使用している場合は登録されません。"
+      if @worker.working_floor == nil || @worker.sign_name == ""
+        flash[:danger] = "未入力項目があります。"
+      else
+        flash[:success] = "#{@worker.working_floor}階職員「#{@worker.worker_name}」さんを新規登録しました。ふりがなに全角空白と半角英数字は使用している場合は登録されません。"
+      end
     else
       flash[:danger] = "入力項目に誤りがあります。担当職員は最大６名までです。"
     end
@@ -32,14 +36,18 @@ class WorkersController < ApplicationController
 
   #職員情報編集モーダル
   def edit_worker
-    @seniors = Senior.using.including_facility
+    @seniors = Senior.using.including_facility.current_facility(current_facility)
   end
 
   #職員情報更新
   def update_worker
     if worker_valid?((worker_params)[:worker_name], (worker_params)[:senior_ids])
       @worker.update_attributes(worker_params)
-      flash[:success] = "職員「#{@worker.worker_name}」さんの情報を更新しました。ふりがなに全角空白と半角英数字は使用している場合は更新されません。"
+      if @worker.sign_name == ""
+        flash[:danger] = "サイン名が未入力です。"
+      else
+        flash[:success] = "#{@worker.working_floor}階職員「#{@worker.worker_name}」さんの情報を更新しました。ふりがなに全角空白と半角英数字は使用している場合は更新されません。"
+      end
     else
       flash[:danger] = "入力項目に誤りがあります。担当職員は最大６名までです。"
     end
@@ -49,7 +57,7 @@ class WorkersController < ApplicationController
   #職員退職ボタン
   def retirement
     if @worker.update_attributes(working_flg: false)
-      flash[:warning] = "職員「#{@worker.worker_name}」さんを退職へ変更しました。"
+      flash[:warning] = "#{@worker.working_floor}階職員「#{@worker.worker_name}」さんを退職へ変更しました。退職者一覧を確認して下さい。"
     end
     redirect_to facility_workers_url
   end
@@ -57,7 +65,7 @@ class WorkersController < ApplicationController
   #職員再就業ボタン
   def re_employment
     if @worker.update_attributes(working_flg: true)
-      flash[:success] = "職員「#{@worker.worker_name}」さん（#{@worker.working_floor}階）を再就業へ変更しました。"
+      flash[:success] = "職員「#{@worker.worker_name}」さんを再就業へ変更しました。#{@worker.working_floor}階を確認して下さい。"
     end
     redirect_to facility_workers_url
   end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -11,6 +11,7 @@ class Worker < ApplicationRecord
   VALID_WORKER_NAME_CALL_REGEX = /\A[ぁ-んー－]+\z/
   validates :worker_name_call, length: { in: 1..25 }, format: { with: VALID_WORKER_NAME_CALL_REGEX }, allow_blank: true
   validates :sign_name, presence: true, length: { in: 1..6 }
+  validates :working_floor, presence: true
 
   #役職選択
   enum position: {
