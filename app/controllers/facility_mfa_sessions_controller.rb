5f75a3c94db9915cce406987215e831ed0627fe8
Hayato Saito <omimisama@gmail.com>
2020-06-09 22:27:38 +0900

    #104_QRコード表示非表示機能

--- app/controllers/facility_mfa_sessions_controller.rb
+++ app/controllers/facility_mfa_sessions_controller.rb
@@ -8,7 +8,6 @@ class FacilityMfaSessionsController < ApplicationController
   def create
     @facility = current_facility
     if @facility.google_authentic?(params[:auth][:mfa_code])
-      raise
       FacilityMfaSession.create(@facility)
       if @facility.email == "center@email.com"
         flash[:success] = "テストユーザー「社会福祉法人うみの風」さんでログインしました。"
@@ -22,4 +21,23 @@ class FacilityMfaSessionsController < ApplicationController
     end
   end
 
+  def update
+    @facility = current_facility
+    if @facility.google_authentic?(params[:auth][:mfa_code])
+      if @facility.display == true
+        if @facility.update_attributes(display: false)
+          flash[:success] = "QRコードを非表示に変更しました。"
+          redirect_to new_facility_mfa_session_url
+        end
+      else
+        if @facility.update_attributes(display: true)
+          flash[:warning] = "QRコードを表示します。スキャン後、非表示にする事をお勧めします。"
+          redirect_to new_facility_mfa_session_url
+        end
+      end
+    else
+      flash[:danger] = "認証コードが間違っています。"
+      redirect_to new_facility_mfa_session_url
+    end
+  end
 end
