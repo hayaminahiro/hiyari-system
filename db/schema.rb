989c2fa0d178dfa984ba59545c4c7868ea759787
Hayato Saito <omimisama@gmail.com>
2020-06-12 19:09:11 +0900

    #106_二段階認証可否カラムでロイグイン可能に

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_070639) do
+ActiveRecord::Schema.define(version: 2020_06_12_094658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_070639) do
     t.string "salt"
     t.string "google_secret"
     t.boolean "display", default: true
+    t.boolean "authenticator_check", default: false
     t.index ["email"], name: "index_facilities_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Facility.create!(name:  "社会福祉法人くろねこ協会",
                  email: "admin_cat@email.com",
                  password:              "password",
                  password_confirmation: "password",
-                 admin: true
+                 admin: true,
 )
 
 # Sampleで多数のデータ投入
@@ -12,6 +12,7 @@ Facility.create!(name:  "社会福祉法人うみの風",
                  email: "center@email.com",
                  password:              "password",
                  password_confirmation: "password",
+                 authenticator_check: true
 )
 
 Facility.create!(name:  "特別養護老人ホーム ねこの街",
